e499efe720da0dd19243f7846244b3e5895f96b0
ajalaj <ajalaj@AJALAJ-IN.oradev.oraclecorp.com>
2021-04-16 16:41:00 +0530

    third

--- src/content/Project.js
+++ src/content/Project.js
@@ -10,17 +10,17 @@ const Project=()=>{
         <div className="project">
         <h1>Projects</h1>
         <div className="project-indi glass">
-        <a href="https://ajalaj-weather-application.herokuapp.com/" target="_blank"><img src={weather} className="project-photo"></img></a>
+        <a href="https://ajalaj-weather-application.herokuapp.com/" target="_blank" rel="noreferrer"><img src={weather} className="project-photo"></img></a>
         <p>Weather app that makes API calls to mapbox.com & weatherstack.com for user entered input.</p>
         </div>
         <br></br>
         <div className="project-indi glass">
-        <a href="https://frosty-boyd-f33901.netlify.app/" target="_blank"><img src={todo} className="project-photo"></img></a>
+        <a href="https://frosty-boyd-f33901.netlify.app/" target="_blank" rel="noreferrer"><img src={todo} className="project-photo"></img></a>
         <p>ReactJs app to help you pick what to do.</p>
         </div>
         <br></br>
         <div className="project-indi glass">
-        <a href="https://to-do-app-e2e99.web.app/" target="_blank"><img src={firebase} className="project-photo"></img></a>
+        <a href="https://to-do-app-e2e99.web.app/" target="_blank" rel="noreferrer"><img src={firebase} className="project-photo"></img></a>
         <p>A ReactJS To-do App with Firebase Authentication</p>
         </div>
 
